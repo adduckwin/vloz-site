2d57d64a11564173dac4b04c897b9aa3cba4d30a
VLOZ <ad@duckwin.ru>
2026-05-31 23:23:19 +0300

    Yandex.Metrika 109550359 — подключена аналитика

--- build/patch.py
+++ build/patch.py
@@ -437,16 +437,17 @@ CSS5 = """
 rep('</style>', CSS5, 'css5')
 
 # analytics (placeholders to fill in)
-ANALYTICS = '''<!-- ВСТАВЬТЕ ID СЧЁТЧИКОВ НИЖЕ -->
-<!-- Yandex.Metrika counter -->
+ANALYTICS = '''<!-- Yandex.Metrika counter -->
 <script type="text/javascript">
-(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-m[i].l=1*new Date();for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}
-k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-(window,document,"script","https://mc.yandex.ru/metrika/tag.js","ym");
-ym(00000000, "init", {clickmap:true,trackLinks:true,accurateTrackBounce:true});  /* <-- ваш номер счётчика Яндекс.Метрики */
+(function(m,e,t,r,i,k,a){
+m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+m[i].l=1*new Date();
+for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}
+k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+})(window,document,"script","https://mc.yandex.ru/metrika/tag.js?id=109550359","ym");
+ym(109550359,"init",{ssr:true,webvisor:true,clickmap:true,ecommerce:"dataLayer",referrer:document.referrer,url:location.href,accurateTrackBounce:true,trackLinks:true});
 </script>
-<noscript><div><img src="https://mc.yandex.ru/watch/00000000" style="position:absolute;left:-9999px" alt=""></div></noscript>
+<noscript><div><img src="https://mc.yandex.ru/watch/109550359" style="position:absolute;left:-9999px" alt=""></div></noscript>
 <!-- /Yandex.Metrika counter -->
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script>
